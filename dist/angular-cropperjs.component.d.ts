b1d06555cfb9501968de0aafa3c155aea5e1138f
Daniel Horon <danyhoron@yahoo.com>
2017-11-03 11:52:45 +0200

    Forgot to compile before doing the other commit.

--- dist/angular-cropperjs.component.d.ts
+++ dist/angular-cropperjs.component.d.ts
@@ -10,6 +10,48 @@ export interface ImageCropperResult {
     blob?: Blob;
     dataUrl?: string;
 }
+/**
+ * If you intend to get a JPEG image from the output canvas, you should set the fillColor option first,
+ * if not, the transparent part in the JPEG image will become black by default.
+ */
+export interface ImageCropperCroppedCanvasOptions {
+    /**
+     * the destination width of the output canvas.
+     */
+    width?: number;
+    /**
+     *  the destination height of the output canvas.
+     */
+    height?: number;
+    /**
+     * the minimum destination width of the output canvas, the default value is 0.
+     */
+    minWidth?: number;
+    /**
+     * the minimum destination height of the output canvas, the default value is 0.
+     */
+    minHeight?: number;
+    /**
+     * the maximum destination width of the output canvas, the default value is Infinity.
+     */
+    maxWidth?: number;
+    /**
+     * the maximum destination height of the output canvas, the default value is Infinity.
+     */
+    maxHeight?: number;
+    /**
+     * a color to fill any alpha values in the output canvas, the default value is transparent.
+     */
+    fillColor?: string;
+    /**
+     * set to change if images are smoothed(true, default ) or not(false).
+     */
+    imageSmoothingEnabled?: boolean;
+    /**
+     * set the quality of image smoothing, one of "low"(default ), "medium", or "high".
+     */
+    imageSmoothingQuality?: string;
+}
 export declare class AngularCropperjsComponent {
     image: ElementRef;
     imageUrl: any;
@@ -37,5 +79,5 @@ export declare class AngularCropperjsComponent {
      * Export canvas
      * @param {string} base64
      */
-    exportCanvas(base64?: any): void;
+    exportCanvas(base64?: boolean, canvasOptions?: ImageCropperCroppedCanvasOptions): void;
 }
