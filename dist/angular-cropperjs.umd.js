91b37381a940228031b01485b07f2c4b27f638e5
Daniel Horon <danyhoron@yahoo.com>
2017-11-03 13:12:34 +0200

    Added /dist to module

--- dist/angular-cropperjs.umd.js
+++ dist/angular-cropperjs.umd.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/core'), require('@angular/common'), require('cropperjs')) :
 	typeof define === 'function' && define.amd ? define(['exports', '@angular/core', '@angular/common', 'cropperjs'], factory) :
-	(factory((global['angular-cropperjs'] = {}),global.core,global.common,global.Cropper));
+	(factory((global['angular-cropperjs/dist'] = {}),global.core,global.common,global.Cropper));
 }(this, (function (exports,core,common,Cropper) { 'use strict';
 
 /* tslint:disable */
